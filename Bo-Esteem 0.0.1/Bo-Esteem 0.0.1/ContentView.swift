ba3631612144d24562f4d883149e4faa9a0eb7ae
lavaspoon <lavaspoon@naver.com>
2019-11-01 16:44:18 +0900

    start

---
+++ Bo-Esteem 0.0.1/Bo-Esteem 0.0.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bo-Esteem 0.0.1
+//
+//  Created by lavaspoon on 2019/11/01.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
